4ead4dae1b13f795254ea48813ac9c00f728881d
Kwan Tecnología <kwantecnologia@Kwans-MacBook-Pro-2.local>
2019-03-06 16:32:38 -0600

    Sign in

---
+++ src/test/java/RegistroUsuario.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegistroUsuario {
+    WebDriver driver;
+    String baseURL;
+    @BeforeClass
+    public void  setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseURL);
+
+
+    }
+    @AfterClass
+    public void cleanUp(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void login(){
+        
+    }
+
+}
